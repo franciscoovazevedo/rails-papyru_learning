e0dc054ccdf1ac12840f58b8c1b7e0f3719b5311
franciscoazevedo <franciscoovazevedo@gmail.com>
2017-08-15 15:22:39 +0100

    almost working with the pictures-profile

--- db/migrate/20170815123315_add_photo_to_user.rb
+++ db/migrate/20170815123315_add_photo_to_user.rb
@@ -1,5 +1,5 @@
 class AddPhotoToUser < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :photo, :string
+    add_column :users, :photo, :string, default: "profile_default_usd3dj"
   end
 end
