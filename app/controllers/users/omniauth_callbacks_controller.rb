761d30b721819ca4f0946b61c74a0914e2f4edc4
Anina Zimmer <a.zimmer@jacobs-university.de>
2017-08-17 16:11:55 +0100

    Completed Facebook backend

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,7 +2,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     user = User.find_for_facebook_oauth(request.env['omniauth.auth'])
     if user.persisted?
-      sign_in_and_redirect user, event: :authentication
+      sign_in user, event: :authentication
+      redirect_to edit_user_registration_path
       set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
     else
       session['devise.facebook_data'] = request.env['omniauth.auth']
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users,
-    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+    controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations' }
   devise_scope :user do
     get 'became_teacher', to: 'confirmations/registrations#became_teacher'
   end
   # get 'became_teacher', to: 'confirmations/registrations#became_teacher'
   resources :teacher_subjects
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'help', to: 'pages#help'
+  get 'about', to: 'pages#about'
+   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
