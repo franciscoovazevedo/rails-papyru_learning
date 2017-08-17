dccbb472e540eb7a28ff00364b697a574ec53bf3
franciscoazevedo <franciscoovazevedo@gmail.com>
2017-08-17 16:25:09 +0100

    user profile BORN

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   has_many :lessons
   has_many :reviews
   has_many :teacher_subjects
+  has_one :profile
   # has_many :lessons
   # has_many :reviews
   # has_many teacher_subjects if teacher == true
@@ -14,7 +15,7 @@ class User < ApplicationRecord
 
   validates :name, :address, :phone_number, presence: true # maybe validate presence after for teacher
   validates_format_of :phone_number, :with => /\A(\+351)?(9|2)\d{8}\z/i
-  
+
   def self.find_for_facebook_oauth(auth)
     user_params = auth.slice(:provider, :uid)
     user_params.merge! auth.info.slice(:email, :first_name, :last_name)
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users,
-    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+    controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: "confirmations/registrations" }
   devise_scope :user do
     get 'became_teacher', to: 'confirmations/registrations#became_teacher'
   end
   # get 'became_teacher', to: 'confirmations/registrations#became_teacher'
   resources :teacher_subjects
   root to: 'pages#home'
-
+  resources :profiles, only: [:show ]
 
   get 'errors', to: 'pages#errors'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
