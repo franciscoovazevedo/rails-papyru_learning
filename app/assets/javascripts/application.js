5c07d9a62a57ce9215f755818091628750f31e08
Gabriele Canepa <gabrielecanepa@icloud.com>
2017-08-17 16:46:06 +0100

    Style first part of login form

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -60,3 +60,19 @@ function scaleBannerVideoSize(element){
     $('.banner .video-container video').addClass('fadeIn animated');
   });
 }
+
+$( ".input" ).focusin(function() {
+  $( this ).find( "span" ).animate({"opacity":"0"}, 200);
+});
+
+$( ".input" ).focusout(function() {
+  $( this ).find( "span" ).animate({"opacity":"1"}, 300);
+});
+
+$(".login").submit(function(){
+  $(this).find(".submit i").removeAttr('class').addClass("fa fa-check").css({"color":"#fff"});
+  $(".submit").css({"background":"#2ecc71", "border-color":"#2ecc71"});
+  $(".feedback").show().animate({"opacity":"1", "bottom":"-80px"}, 400);
+  $("input").css({"border-color":"#2ecc71"});
+  return false;
+});
