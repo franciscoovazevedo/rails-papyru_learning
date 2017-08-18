77c667df00182fbfc8adf555ce28b2593f9cefad
franciscoazevedo <franciscoovazevedo@gmail.com>
2017-08-18 14:51:15 +0100

    reviews done - FRANCISCO

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :user
+  belongs_to :lesson
 
   validates :rating, presence: true, numericality: true
   validates :content, presence: true
---
+++ db/migrate/20170818130858_add_lesson_to_reviews.rb
@@ -0,0 +1,5 @@
+class AddLessonToReviews < ActiveRecord::Migration[5.0]
+  def change
+    add_reference :reviews, :lesson, foreign_key: true
+  end
+end
