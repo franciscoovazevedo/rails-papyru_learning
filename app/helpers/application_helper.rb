761e3285d5bc7c12e61eb7ce93051cb35599f8a1
franciscoazevedo <franciscoovazevedo@gmail.com>
2017-08-18 15:27:21 +0100

    profile page done

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def user_avatar(user)
+    if user.photo_url.present?
+      user.photo_url
+    elsif user.facebook_picture_url.present?
+      user.facebook_picture_url
+    else
+      "https://www.1plusx.com/app/mu-plugins/all-in-one-seo-pack-pro/images/default-user-image.png"    end
+  end
 end
